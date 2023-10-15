06e65110e3c46affea825b120719612933626b5e
satish <85542793+satish-hebbal@users.noreply.github.com>
2023-10-15 14:01:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        pop :" 'Poppins', sans-serif",
+        rale : "'Raleway', sans-serif" 
+      },
+    },
+  },
+  plugins: [],
+}
+
